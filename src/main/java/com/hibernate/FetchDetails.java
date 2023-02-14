43bedbee2b3b42202f4057bceece4dbc248121df
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-14 22:32:26 +0530

    get and load methods

--- src/main/java/com/hibernate/FetchDetails.java
+++ src/main/java/com/hibernate/FetchDetails.java
@@ -1,5 +1,29 @@
 package com.hibernate;
 
-public class FetchDetails {
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class FetchDetails {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("config.xml");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		//get id 1
+		Student student = session.get(Student.class, 1);
+//		Student student2 = session.get(Student.class, 2);
+		Student student2 = session.load(Student.class, 2);
+		System.out.println(student);
+		System.out.println(student2);
+//		
+		session.close();
+		sessionFactory.close();
+	}
 }
--- src/main/java/com/hibernate/Student.java
+++ src/main/java/com/hibernate/Student.java
@@ -1,9 +1,7 @@
 package com.hibernate;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 //to change whole entity name
 //@Entity(name = "anotherName")
