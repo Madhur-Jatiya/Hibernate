7903f0210f51317a533be5c140ee9f1225d8dbf9
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-14 21:08:50 +0530

    data insert into db using hibernate

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,8 +1,27 @@
 package com.hibernate;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello India!" );
-    }
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("config.xml");
+//		cfg.configure("config.xml").addAnnotatedClass(Student.class);
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+
+		Student student = new Student();
+		student.setId(3);
+		student.setName("Rani");
+		student.setCity("Indore");
+		System.out.println(student);
+
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(student);
+		tx.commit();
+		session.close();
+	}
 }
---
+++ src/main/java/com/hibernate/Student.java
@@ -0,0 +1,43 @@
+package com.hibernate;
+
+//to change whole entity name
+//@Entity(name = "anotherName")
+//to change table name default name is class name
+//@Table(name = "tableName")
+
+@jakarta.persistence.Entity
+public class Student {
+	@jakarta.persistence.Id
+	private int id;
+	private String name;
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
