509d2738094a7c4be91c57b05bed2a0ea937a8c3
Madhur <madhurmoms@gmail.com>
2023-03-23 14:45:04 +0530

    one to one and many to one

---
+++ src/main/java/com/hibernate/OneToMany/App.java
@@ -0,0 +1,52 @@
+package com.hibernate.OneToMany;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) throws IOException {
+		Configuration cfg = (Configuration) new Configuration().configure("config.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tr = session.beginTransaction();
+
+		// insert
+		Country country = new Country();
+		country.setName("India");
+
+		City city = new City();
+		city.setName("Bhopal");
+		city.setPin(462001);
+
+		City city2 = new City();
+		city2.setName("Jaipur");
+		city2.setPin(302001);
+
+		List<City> cityList = new ArrayList<City>();
+		cityList.add(city);
+		cityList.add(city2);
+
+		country.setCity(cityList);
+
+		// Extract
+//		List<Country> getIntern = session.createQuery("from Intern").list();
+//		for (Country intern2 : getIntern) {
+//			System.out.println(intern2);
+//		}
+
+		session.save(country);
+		session.save(city);
+		session.save(city2);
+
+		tr.commit();
+
+		session.close();
+		sf.close();
+	}
+}
---
+++ src/main/java/com/hibernate/OneToMany/City.java
@@ -0,0 +1,61 @@
+package com.hibernate.OneToMany;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "City")
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "Name")
+	private String name;
+	@Column(name = "Pin Code")
+	private int pin;
+
+	public int getId() {
+		return id;
+	}
+
+	public City(int id, String name, int pin) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.pin = pin;
+	}
+
+	public City() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "City [id=" + id + ", name=" + name + ", pin=" + pin + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
